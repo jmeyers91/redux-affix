5074677152c7b37542fc8c9c0c007e454e0efb63
James Meyers <james.meyers@emberex.com>
2017-04-03 14:27:33 -0700

    Add files.

---
+++ README.md
@@ -0,0 +1,31 @@
+# redux-affix
+
+A react-redux's connect function with additional shorthand syntax.
+
+# Usage
+
+`affix` behaves exactly like React-Redux's `connect` function when `mapStateToProps` and `mapDispatchToProps` are functions.
+
+Pass single state key as `mapStateToProps`
+
+```javascript
+affix('todos')
+```
+
+Pass state keys as `mapStateToProps`
+
+```javascript
+affix(['todos', 'user'])
+```
+
+Pass state paths as `mapStateToProps`
+
+```javascript
+affix({username: 'user.name', todoCount: 'todos.length'})
+```
+
+Pass action creators as `mapDispatchToProps`
+
+```javascript
+affix(mapStateToProps, {onToggleTask: toggleTask})
+```
---
+++ src/redux-affix.js
@@ -0,0 +1,77 @@
+import { connect } from 'react-redux';
+import get from 'lodash.get';
+
+// connect('*')
+function identity(value) {
+  return value;
+}
+
+// connect(['some', 'anotherKey'])
+function mapStateToPropsFromKeys(keys) {
+  return function mapStateToProps(state) {
+    return keys.reduce((props, key) => {
+      props[key] = state[key];
+      return props;
+    }, {});
+  }
+}
+
+// connect({propKey: 'some.store.state.path'})
+function mapStateToPropsFromMap(map) {
+  return function mapStateToProps(state) {
+    return Object.keys(map).reduce((props, key) => {
+      const path = map[key];
+      props[key] = get(state[key], path);
+      return props;
+    }, {});
+  }
+}
+
+// connect(mapStateToProps, {someProp: someActionCreator})
+function mapDispatchToPropsFromMap(map) {
+  const propNames = Object.keys(map);
+  return function mapDispatchToProps(dispatch, ownProps) {
+    return propNames.reduce((props, propName) => {
+      const actionCreator = map[propName];
+      props[propName] = function(...args) {
+        dispatch(actionCreator(...args, ownProps));
+      };
+      return props;
+    }, {});
+  }
+}
+
+function wrapMapStateToProps(mapStateToProps) {
+  if(mapStateToProps) {
+    const type = typeof mapStateToProps;
+    if(type === 'string') {
+      return mapStateToPropsFromKeys([mapStateToProps]);
+    } else if(Array.isArray(mapStateToProps)) {
+      return mapStateToPropsFromKeys(mapStateToProps);
+    } else if(type === 'object') {
+      return mapStateToPropsFromMap(mapStateToProps);
+    }
+  }
+  return mapStateToProps;
+}
+
+function wrapMapDispatchToProps(mapDispatchToProps) {
+  if(mapDispatchToProps && typeof mapDispatchToProps === 'object') {
+    return mapDispatchToPropsFromMap(mapDispatchToProps);
+  }
+  return mapDispatchToProps;
+}
+
+// for testing
+export function wrapConnect(connect) {
+  return function affix(mapStateToProps, mapDispatchToProps, mergeProps, options) {
+    return connect(
+      wrapMapStateToProps(mapStateToProps),
+      wrapMapDispatchToProps(mapDispatchToProps),
+      mergeProps,
+      options
+    );
+  }
+}
+
+export default wrapConnect(connect);
